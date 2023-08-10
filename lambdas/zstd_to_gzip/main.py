c6b356ecf33e1a8da748eab07290905dcf9c89cb
Diego Perez <pharesdiego@gmail.com>
2023-08-09 21:17:51 -0500

    feat: add mvp for zstd_to_gzip lambda

---
+++ lambdas/zstd_to_gzip/main.py
@@ -0,0 +1,25 @@
+import boto3
+import pandas as pd
+from typing import TypedDict
+from io import BytesIO
+
+class ExpectedEvent(TypedDict):
+    file_key: str
+    bucket_name: str
+
+def handler(event: ExpectedEvent, context):
+    """
+    Convert and overwrites file in S3 from ZSTD compression to GZIP.
+    """
+    s3 = boto3.client('s3')
+
+    response = s3.get_object(Bucket=event['bucket_name'], Key=event['file_key'])
+
+    zstd_compressed_data = response['Body'].read()
+
+    temp_file = '/tmp/data.parquet'
+
+    with open(temp_file, 'wb+') as tf:
+        pd.read_parquet(BytesIO(zstd_compressed_data)).to_parquet(tf, compression='gzip', engine='fastparquet')
+
+    s3.upload_file(Bucket=event['bucket_name'], Key=event['file_key'], Filename=temp_file)
